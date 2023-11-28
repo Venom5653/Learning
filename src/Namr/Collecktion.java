e1ca2bbc5e66447f0924750f516c235d459ed1a7
Danila Mihajlov <Mihajlovvenom@gmail.com>
2023-11-28 19:33:09 +0700

    Addddd

--- src/Namr/Collecktion.java
+++ src/Namr/Collecktion.java
@@ -2,10 +2,11 @@ package Namr;
 
 public class Collecktion {
     public static void main(String[] args) {
-        
+        Collecktion collecktion = new Collecktion();
+        System.out.println(collecktion.sum());
     }
 
-    public int sum(){
+    public int sum() {
         int g = 0;
         int v = 1;
         int b = g + v;
